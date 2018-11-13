eef90148cd045f973d5b96527a2e8ccf328aacd1
mrvdark <m.r.v.leo.dark@gmail.com>
2018-11-13 08:31:38 +0700

    done

---
+++ src/FizzBuzz.java
@@ -0,0 +1,21 @@
+
+public class FizzBuzz {
+
+    private static boolean isFizz;
+    private static boolean isBuzz;
+
+    public static String fizzBuzz(int number) {
+        isFizz = number % 3 == 0;
+        isBuzz = number % 5 == 0;
+        if (isFizz && isBuzz)
+            return "FizzBuzz";
+
+        if (isFizz)
+            return "Fizz";
+
+        if (isBuzz)
+            return "Buzz";
+
+        return number + "";
+    }
+}
---
+++ src/FizzBuzzTest.java
@@ -0,0 +1,9 @@
+
+public class FizzBuzzTest {
+    public static void main(String[] args) {
+        System.out.println("1 is " + FizzBuzz.fizzBuzz(1));
+        System.out.println("2 is " + FizzBuzz.fizzBuzz(2));
+        System.out.println("3 is " + FizzBuzz.fizzBuzz(3));
+        System.out.println("4 is " + FizzBuzz.fizzBuzz(4));
+    }
+}
